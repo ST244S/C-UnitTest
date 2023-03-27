a276e9b8d2975875d8a6e1d8479581d9a100a694
Jiawei.Ye <Jiawei.Ye@saultech.com.tw>
2023-03-27 11:36:50 +0800

    C#Test

---
+++ Jenkins_HellowWorld測試用/Jenkins_HellowWorld測試用/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Jenkins_HellowWorld測試用
+{
+     public  class Program
+    {
+        public  static void Main()
+        {
+            Console.WriteLine("Hello World! form Thetips4you");
+        }
+    }
+}
